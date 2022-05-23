f95c85f95afa2bb89f730806e0f193033ec7ccf5
SanskarPoudel <72004392+SanskarPoudel@users.noreply.github.com>
2022-05-23 17:22:15 +0545

    first commit

--- src/App.js
+++ src/App.js
@@ -2,30 +2,26 @@ import "./App.css";
 import Card from "./components/Card";
 import HeroSection from "./components/HeroSection";
 import Navbar from "./components/Navbar";
-import data from "./components/data.js"
+import data from "./components/data.js";
 function App() {
-  const cards = data.map(items => {
+  const cards = data.map((items) => {
     return (
       <Card
-       image={items.image}
-          status={items.status}
-          rating={items.rating}
-          rating_no={items.rating_no}
-          country={items.country}
-          description={items.description}
-          text={items.text}
+        image={items.image}
+        status={items.status}
+        rating={items.rating}
+        rating_no={items.rating_no}
+        country={items.country}
+        description={items.description}
+        text={items.text}
       />
-    )
-  })
+    );
+  });
   return (
     <div className="App">
       <Navbar />
       <HeroSection />
-      <div className="cards">
-       
-        {cards}
-        
-      </div>
+      <div className="cards">{cards}</div>
     </div>
   );
 }
